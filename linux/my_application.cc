46876672311289e927bffa1464a1cb74d6783ce4
Davidt <DavidTrujiTorres@gmail.com>
2023-11-11 14:25:59 +0100

    Fix launcher icons

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "sunflower_flutter");
+    gtk_header_bar_set_title(header_bar, "Sunflower - Flutter version");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "sunflower_flutter");
+    gtk_window_set_title(window, "Sunflower - Flutter version");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
